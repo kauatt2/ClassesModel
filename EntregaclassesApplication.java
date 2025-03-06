f6112d254900e50c9ca88f38599cceaca03c1428
kaka <166547922+kauatt2@users.noreply.github.com>
2025-03-06 18:47:48 -0300

    Add files via upload

---
+++ EntregaclassesApplication.java
@@ -0,0 +1,14 @@
+
+package com.kakadumal.entregaclasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaclassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaclassesApplication.class, args);
+	}
+
+}
